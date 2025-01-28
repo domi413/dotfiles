1debe1ab155112a6fcbde913511102ba326f7351
domi413 <dom.buehler@outlook.com>
2025-01-28 19:27:27 +0100

    update flash nvim

--- nvim_minimal/lua/plugins/flash.lua
+++ nvim_minimal/lua/plugins/flash.lua
@@ -1,14 +1,49 @@
 return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
-	---@type Flash.Config
-	opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+	opts = {
+
+		jump = {
+			autojump = false,
+		},
+		label = {
+			upperase = false,
+			format = function(opts)
+				return { { string.lower(opts.match.label), opts.hl_group } }
+			end,
+		},
+		highlight = {
+			-- Keep normal highlighting
+			backdrop = false,
+		},
+		modes = {
+			char = {
+				-- Disable f,F,t,T motions
+				enabled = false,
+			},
+		},
+		prompt = {
+			enabled = false,
+		},
+	},
+	keys = {
+		{
+			-- Jump through code
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			-- Select code fragments
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+	},
 }
