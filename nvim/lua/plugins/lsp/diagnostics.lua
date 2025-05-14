83ad28f27a7d6bcd3a0d178619a7df2dbbb3c343
domi413 <dom.buehler@outlook.com>
2025-05-14 11:00:05 +0200

    ..

--- nvim/lua/plugins/lsp/diagnostics.lua
+++ nvim/lua/plugins/lsp/diagnostics.lua
@@ -1,17 +1,20 @@
 return {
 	"maan2003/lsp_lines.nvim",
 	config = function()
-		vim.diagnostic.config({ virtual_text = true })
-		local virtual_lines_enabled = false
+		local diagnostic = vim.diagnostic.config
 
+		diagnostic({ virtual_text = { true, prefix = "●" } })
+
+		local virtual_lines_enabled = false
 		vim.keymap.set("n", "<Leader>dl", function()
 			virtual_lines_enabled = not virtual_lines_enabled
 
 			if virtual_lines_enabled then
-				vim.diagnostic.config({ virtual_text = false })
+				diagnostic({ virtual_text = false })
 				require("lsp_lines").toggle(true)
 			else
-				vim.diagnostic.config({ virtual_text = true })
+				-- diagnostic({ virtual_text = true })
+				diagnostic({ virtual_text = { true, prefix = "●" } })
 				require("lsp_lines").toggle(false)
 			end
 		end, { desc = "Detailed diagnostics" })
